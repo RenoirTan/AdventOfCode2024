f5861d0afa889d57b532254d92e1f28d0a5f7841
RenoirTan <renoirtan2005@gmail.com>
2024-12-22 11:08:06 +0800

    main: script to run all challenges

---
+++ run-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+function everything() {
+    for i in {1..19}; do
+        day=$(printf "%02d" $i)
+        env -C $day/ make run-p1
+        env -C $day/ make run-p2
+    done
+}
+
+everything
